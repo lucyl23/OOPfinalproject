cd01856e162e93910f694b226930a0f4185527cf
chiachen <chiachen504@gmail.com>
2026-05-07 21:54:35 +0800

    Update maincode.java

--- maincode.java
+++ maincode.java
@@ -1,1 +1,94 @@
+import java.util.Scanner;
+class Character{
+    protected String name;
+    protected int HP;
+    protected int ATK;
+    protected int Ult;
+
+    Character(String name, int HP, int ATK, int Ult){
+        this.name = name;
+        this.HP = HP;
+        this.ATK = ATK;
+        this.Ult = Ult;
+    }
+
+    void beAttack(int ATK){
+    }
+
+    void beAttack(String ult){
+    }
+}
+
+class Friend extends Character{
+    int healTimes = 2;
+
+    Friend(String name, int HP, int ATK, int Ult, int healTimes){
+        super(name, HP, ATK, Ult);
+        this.healTimes = healTimes;
+    }
+
+    @Override
+    void beAttack(int ATK){
+        this.HP -= ATK;
+    }
+
+    @Override
+    void beAttack(String ult){
+        this.HP -= 50;
+    }
+
+    void heal(){
+        this.HP += 20;
+        this.healTimes -= 1;
+    }
+
+}
+
+class Boss extends Character{
+    int fullyRecoveredTimes = 1;
+
+    Boss(String name, int HP, int ATK, int Ult, int fullyRecoveredTimes){
+        super(name, HP, ATK, Ult);
+        this.fullyRecoveredTimes = fullyRecoveredTimes;
+    }
+
+    @Override
+    void beAttack(int ATK){
+        this.HP -= ATK;
+    }
+
+    @Override
+    void beAttack(String ult){
+        this.HP -= 50;
+    }
+
+    void fullyRecovered(){
+        fullyRecoveredTimes -= 1;
+        this.HP = 100;
+    }
+}
+
+public class buildClass {
+    public static void main(String[] args){
+        //使用者選擇角色
+        System.out.println("角色列表 \n A 爆豪勝己 / B 成步堂龍一 百戰百勝 / C 宮野真守 殘念王子系之心中神 \n 請輸入代碼選擇角色");
+        Scanner scn = new Scanner(System.in);
+        String chooceCharacter = scn.next();
+        switch (chooceCharacter){
+            case "A":
+
+                break;
+
+            case "B":
+                break;
+
+            case "C":
+                break;
+        }
+         
+
+
+        //建立物件
+    }
+}
 

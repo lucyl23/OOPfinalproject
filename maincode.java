7aa9c5d873167fbede6fcd04b4e67ad4fbad57b2
chiachen <chiachen504@gmail.com>
2026-05-07 22:06:13 +0800

    Update maincode.java

--- maincode.java
+++ maincode.java
@@ -75,21 +75,19 @@ public class FIGHT {
         System.out.println("角色列表 \n A 爆豪勝己 / B 成步堂龍一 百戰百勝 / C 宮野真守 殘念王子系之心中神 \n 請輸入代碼選擇角色");
         Scanner scn = new Scanner(System.in);
         String chooceCharacter = scn.next();
+
+        //建立物件
         switch (chooceCharacter){
             case "A":
-
+                Friend user = new Friend("爆豪勝己", 130, 30,0, 3);
                 break;
 
             case "B":
+                Friend user = new Friend("成步堂龍一", 150, 40,0, 2);
                 break;
 
             case "C":
+                Friend user = new Friend("宮野真守", 200, 20,0, 2);
                 break;
-        }
-         
-
-
-        //建立物件
-    }
 }
 

679aa4e4e221f5da95310f578aedc0e04072d86c
chiachen <chiachen504@gmail.com>
2026-05-07 23:12:49 +0800

    Update maincode.java

--- maincode.java
+++ maincode.java
@@ -3,13 +3,13 @@ class Character{
     protected String name;
     protected int HP;
     protected int ATK;
-    protected int Ult;
+    protected int UltATK;
 
-    Character(String name, int HP, int ATK, int Ult){
+    Character(String name, int HP, int ATK, int UltATK){
         this.name = name;
         this.HP = HP;
         this.ATK = ATK;
-        this.Ult = Ult;
+        this.UltATK = UltATK;
     }
 
     void beAttack(int ATK){
@@ -17,14 +17,27 @@ class Character{
 
     void beAttack(String ult){
     }
+
+    String getName(){
+        return this.name;
+    }
+    int getHP(){
+        return this.HP;
+    }
+    int getAttack(){
+        return this.ATK;
+    }
+    
 }
 
 class Friend extends Character{
     int healTimes = 2;
+    int ultCounter;
 
-    Friend(String name, int HP, int ATK, int Ult, int healTimes){
-        super(name, HP, ATK, Ult);
+    Friend(String name, int HP, int ATK, int UltATK, int healTimes, int ultCounter){
+        super(name, HP, ATK, UltATK);
         this.healTimes = healTimes;
+        this.ultCounter = ultCounter;
     }
 
     @Override
@@ -38,7 +51,7 @@ class Friend extends Character{
     }
 
     void heal(){
-        this.HP += 20;
+        this.HP += 40;
         this.healTimes -= 1;
     }
 
@@ -47,8 +60,8 @@ class Friend extends Character{
 class Boss extends Character{
     int fullyRecoveredTimes = 1;
 
-    Boss(String name, int HP, int ATK, int Ult, int fullyRecoveredTimes){
-        super(name, HP, ATK, Ult);
+    Boss(String name, int HP, int ATK, int UltATK, int fullyRecoveredTimes){
+        super(name, HP, ATK, UltATK);
         this.fullyRecoveredTimes = fullyRecoveredTimes;
     }
 
@@ -64,30 +77,51 @@ class Boss extends Character{
 
     void fullyRecovered(){
         fullyRecoveredTimes -= 1;
-        this.HP = 100;
+        this.HP = 500;
     }
 }
 
-// 檔案名稱：FIGHT
+public void fighting(){
+    //玩家回合開始
+    System.out.println("輪到你的回合！決定你的美妙舞姿吧！\n 請輸入行動編號進行動作呀呼");
+    Scanner scn = new Scanner(System.in);
+    String move = scn.next();
+
+    //badGuy回合開始
+    System.out.println("戦え");
+
+}
+
 public class FIGHT {
     public static void main(String[] args){
         //使用者選擇角色
-        System.out.println("角色列表 \n A 爆豪勝己 / B 成步堂龍一 百戰百勝 / C 宮野真守 殘念王子系之心中神 \n 請輸入代碼選擇角色");
+        System.out.println("角色列表 \n A 爆豪勝己 / B 成步堂龍一 百戰百勝 / C 宮野真守 殘念王子系之心中神 \n 請輸入編號選擇夥伴！");
         Scanner scn = new Scanner(System.in);
         String chooceCharacter = scn.next();
 
         //建立物件
+        Boss badGuy = new Boss("艾連葉卡", 500, 40, 0, 1);
+        
+        Character user = null;
         switch (chooceCharacter){
             case "A":
-                Friend user = new Friend("爆豪勝己", 130, 30,0, 3);
+                user = new Friend("爆豪勝己", 130, 30,80, 5, 4);
                 break;
 
             case "B":
-                Friend user = new Friend("成步堂龍一", 150, 40,0, 2);
+                user = new Friend("成步堂龍一", 150, 40,60, 4, 3);
                 break;
 
             case "C":
-                Friend user = new Friend("宮野真守", 200, 20,0, 2);
+                user = new Friend("宮野真守", 200, 20, 60, 2, 2);
                 break;
-}
+        }
 
+        //戰鬥開始
+        System.out.println("　你就是" + badGuy.getName() + "嗎？！我來找你打架了！納命來！！！");
+        System.out.println("艾連葉卡：敬告所有尤米爾的子民，\n我的名字是艾連葉卡，正透過始祖巨人的力量與所有尤米爾的子民對話。\n帕拉迪島上所有用以打造高牆的硬質化已解除，埋藏其中的所有巨人已經開始行動。\n我的目的是保護我成長的帕拉迪島上的人，但世界不僅希望消滅帕拉迪島上的人，更渴望將所有尤米爾子民趕盡殺絕。\n我拒絕接受他們的期望，城牆裡的巨人將會踏遍這座島以外的大地，直到將所有生命都從這世上驅除殆盡");
+
+        roundEnd();
+        
+    }
+}

e2e32ea2eb28a60e8e9bcecba6c8d68b0246944a
chiachen <chiachen504@gmail.com>
2026-05-14 19:39:58 +0800

    Update maincode.java
    玩家爆擊 血量上限 戰鬥結束的台詞 幸運值 做出角色說話的空間但還沒填詞

--- maincode.java
+++ maincode.java
@@ -43,15 +43,18 @@ class Character{
 
 class Friend extends Character{
     private int luck;
+    private int onceheal = 70;
     private int healTimes = 2;
     private int ultCounter;
     private final int fullUltCounter;
+    private final int fullHP;
     private int powerPoints;
     Friend(String name, int HP, int ATK, int UltATK, int luck, int healTimes, int ultCounter, int powerPoints){
         super(name, HP, ATK, UltATK);
         this.healTimes = healTimes;
         this.ultCounter = ultCounter;
         this.fullUltCounter = ultCounter;
+        this.fullHP = HP;
         this.powerPoints = powerPoints;
     }
     
@@ -65,12 +68,20 @@ class Friend extends Character{
         this.setHP(this.getHP() - ultATK);
     }
 
+    int getFullHP(){
+        return this.fullHP;
+    }
+
     int getHealTimes(){
         return this.healTimes;
     }
 
+    int getOnceHeal(){
+        return this.onceheal;
+    }
+
     void heal(){
-        this.setHP(this.getHP() + 70);
+        this.setHP(this.getHP() + this.onceheal);
         this.healTimes -= 1;
     }
 
@@ -164,10 +175,15 @@ public class FIGHT {
             System.out.println("再" + user.getUltCounter() + "次攻擊後會釋放大招");
         }
     }
-    if(move.equals("2")){
+    if(move.equals("2")){ //回血
         if(user.getHealTimes()>0){
             user.heal();
-            System.out.println("我方回復70點血量");
+            if(user.getHP() > user.getFullHP()){
+                System.out.println("我方回復" + (user.getOnceHeal() - (user.getHP() - user.getFullHP())) + "點血量");
+                user.setHP(user.getFullHP());
+            }
+            System.out.println("我方回復" + user.getOnceHeal() + "點血量");
+            System.out.println("還剩下" + user.getHealTimes() + "瓶靈魂瓶");
         }
         else{
             System.out.println("你的靈魂瓶已用完，你怎麼忘記了，下去。");
@@ -176,7 +192,22 @@ public class FIGHT {
 
     //badGuy回合開始
     user.beAttack(badGuy.getAttack());
-    System.out.println("艾連：塔塔開欸");
+    switch (badGuy.getName()){
+				case "All For One":
+					System.out.println("\n");
+					break;
+				case "艾連葉卡":
+					System.out.println("艾連葉卡：塔塔開欸\n");
+					break;
+				case "宇智波班":
+                    if(Math.random() <= 0.5){
+                        System.out.println("宇智波班：天礙震星\n");
+                    }
+                    else{
+                        System.out.println("宇智波班：完全體須佐能乎\n");
+                    }
+					break;
+			}
     System.out.println("敵方攻擊" + badGuy.getAttack() + "點傷害");
 }
 
@@ -198,6 +229,20 @@ public static void roundEnd(int round, Boss badGuy, Friend user) {
         System.out.println("角色列表 \n 1 爆豪勝己 大・爆・殺・神 Dynamight / 2 成步堂龍一 百戰百勝 / 3 宮野真守 殘念王子系之心中神 \n 請輸入編號選擇夥伴！");
         int chooseFriend = scn.nextInt();
         Friend user = friendList[chooseFriend-1];
+
+        //選角確定台詞
+        switch (user.getName()){
+				case "爆豪勝己":
+					System.out.println("\n");
+					break;
+				case "成步堂龍一":
+					System.out.println("\n");
+					break;
+				case "宮野真守":
+					System.out.println("從今以後，你就要跟你的同伴一起拯救佐賀。我就是那個要把你變成偶像的人！\n");
+					break;
+			}
+
         System.out.println("分配你的實打實攻擊力和幸運值（關乎爆擊與閃避），你有" + user.getPowerPoints() + "點可以分配，輸入你要分配多少給攻擊力，剩下就是幸運值");
         //這邊可以加上下限限制的if-else
         int plusATK = scn.nextInt();
@@ -206,17 +251,27 @@ public static void roundEnd(int round, Boss badGuy, Friend user) {
 
         Boss[] bossList;
         bossList = new Boss[3];
-        bossList[0] = new Boss("艾連葉卡", 500, 40, 0, 1);
+        bossList[0] = new Boss("All For One", 400, 60, 0, 1);
         bossList[1] = new Boss("艾連葉卡", 500, 40, 0, 1);
-        bossList[2] = new Boss("艾連葉卡", 500, 40, 0, 1);
+        bossList[2] = new Boss("宇智波班", 700, 80, 0, 0);
 
-        System.out.println("角色列表 \n 1 艾連葉卡 / 2 艾連葉卡 / 3 艾連葉卡 \n 請輸入編號選擇攻略魔王！");
+        System.out.println("角色列表 \n 1 All For One / 2 艾連葉卡 / 3 宇智波班 \n 請輸入編號選擇攻略魔王！");
         int chooseBoss = scn.nextInt();
         Boss badGuy = bossList[chooseBoss-1];
 
         //戰鬥開始
         System.out.println("玩家：\n你就是" + badGuy.getName() + "嗎？！我來找你打架了！納命來！！！\n");
-        System.out.println("艾連葉卡：\n敬告所有尤米爾的子民，\n我的名字是艾連葉卡，正透過始祖巨人的力量與所有尤米爾的子民對話。\n帕拉迪島上所有用以打造高牆的硬質化已解除，埋藏其中的所有巨人已經開始行動。\n我的目的是保護我成長的帕拉迪島上的人，但世界不僅希望消滅帕拉迪島上的人，更渴望將所有尤米爾子民趕盡殺絕。\n我拒絕接受他們的期望，城牆裡的巨人將會踏遍這座島以外的大地，直到將所有生命都從這世上驅除殆盡。\n");
+        switch (badGuy.getName()){
+				case "All For One":
+					System.out.println("\n");
+					break;
+				case "艾連葉卡":
+					System.out.println("艾連葉卡：\n敬告所有尤米爾的子民，\n我的名字是艾連葉卡，正透過始祖巨人的力量與所有尤米爾的子民對話。\n帕拉迪島上所有用以打造高牆的硬質化已解除，埋藏其中的所有巨人已經開始行動。\n我的目的是保護我成長的帕拉迪島上的人，但世界不僅希望消滅帕拉迪島上的人，更渴望將所有尤米爾子民趕盡殺絕。\n我拒絕接受他們的期望，城牆裡的巨人將會踏遍這座島以外的大地，直到將所有生命都從這世上驅除殆盡。\n");
+					break;
+				case "宇智波班":
+                    System.out.println("宇智波班：天礙震星\n");
+					break;
+			}
         System.out.println("：開始戰鬥吧！");
         System.out.println("==============================");
 
@@ -229,7 +284,17 @@ public static void roundEnd(int round, Boss badGuy, Friend user) {
                 roundEnd(round, badGuy, user);
             }
             else if(user.getHP()>0 && badGuy.getHP()<=0){
-                System.out.println("你贏啦太強啦");
+                switch (badGuy.getName()){
+				case "All For One":
+					System.out.println("All For One：\n");
+					break;
+				case "艾連葉卡":
+					System.out.println("艾連葉卡：\n");
+					break;
+				case "宇智波班":
+					System.out.println("宇智波班：我不過是想在這虛幻的世界上，畫上一個真實的句點。\n");
+					break;
+			}
                 break;
             }
             else if(user.getHP()<=0 && badGuy.getHP()>0){
@@ -241,7 +306,7 @@ public static void roundEnd(int round, Boss badGuy, Friend user) {
         }
 
         //戰鬥結束
-        System.out.println();
+        System.out.println("啊～又是這個夢。");
         //這邊感覺是說點話就好
         scn.close();
     }
